014e43c62fc8f0f1b75d5a51fcd1400f3ddbaf35
Mark Slater <mark.slater@mail.com>
2024-05-21 12:43:46 +0100

    Most basic working example: does nothing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+use rp_pico::entry;
+use panic_halt as _;
+#[entry]
+fn see_doesnt_have_to_be_called_main() -> ! {
+    loop {}
 }
